e07e4f1efd0fb384896a93efbed09933d5fdf84c
Tiago Pires <tiagopgu.pires@gmail.com>
2023-11-13 00:13:46 -0300

    Implementando movimentos possível do bispo e da torre.

--- Xadrez.Console/Xadrez/Pecas/Bispo.cs
+++ Xadrez.Console/Xadrez/Pecas/Bispo.cs
@@ -12,7 +12,37 @@ namespace Xadrez.ConsoleApp.Xadrez.Pecas
 
         public override bool[,] RetornarMovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            var movimentosPossiveis = new bool[Tabuleiro.Linha, Tabuleiro.Coluna];
+
+            // Vertical Superior
+            for (int i = Posicao.Linha; i > 0; i--)
+            {
+                Tabuleiro.Posicao posicao = new(i - 1, i - 1);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+
+                posicao.Coluna = i + 1;
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Vertical Inferior
+            for (int i = Posicao.Linha; i < Tabuleiro.Linha; i++)
+            {
+                Tabuleiro.Posicao posicao = new(i + 1, i - 1);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+
+                posicao.Coluna = i + 1;
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            return movimentosPossiveis;
         }
 
         public override string ToString() => "B";
--- Xadrez.Console/Xadrez/Pecas/Torre.cs
+++ Xadrez.Console/Xadrez/Pecas/Torre.cs
@@ -12,7 +12,27 @@ namespace Xadrez.ConsoleApp.Xadrez.Pecas
 
         public override bool[,] RetornarMovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            var movimentosPossiveis = new bool[Tabuleiro.Linha, Tabuleiro.Coluna];
+
+            // Movimento Vertical
+            for (int i = 0; i < Tabuleiro.Linha; i++)
+            {
+                Tabuleiro.Posicao posicao = new(i, Posicao.Coluna);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Movimento Horizontal
+            for (int i = 0; i < Tabuleiro.Coluna; i++)
+            {
+                Tabuleiro.Posicao posicao = new(Posicao.Linha, i);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            return movimentosPossiveis;
         }
 
         public override string ToString() => "T";
