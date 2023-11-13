e9754abb92638cb38a830dfdfc53106ae3cf861b
Tiago Pires <tiagopgu.pires@gmail.com>
2023-11-13 00:04:02 -0300

    Implementando movimentos válidos da dama.

--- Xadrez.Console/Xadrez/Pecas/Dama.cs
+++ Xadrez.Console/Xadrez/Pecas/Dama.cs
@@ -12,7 +12,57 @@ namespace Xadrez.ConsoleApp.Xadrez.Pecas
 
         public override bool[,] RetornarMovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            var movimentosPossiveis = new bool[Tabuleiro.Linha, Tabuleiro.Coluna];
+
+            // Movimento Vertical
+            for (int i = 0; i < Tabuleiro.Linha; i++)
+            {
+                Tabuleiro.Posicao posicao = new(i, Posicao.Coluna);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[i, Posicao.Coluna] = true;
+            }
+
+            // Movimento Horizontal
+            for (int i = 0; i < Tabuleiro.Coluna; i++)
+            {
+                Tabuleiro.Posicao posicao = new(Posicao.Linha, i);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[Posicao.Linha, i] = true;
+            }
+
+            // Vertical superior
+
+            for (int i = Posicao.Linha; i > 0; i--)
+            {
+                Tabuleiro.Posicao posicao = new(i - 1, i - 1);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+
+                posicao.Coluna = i + 1;
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Vertical inferior
+
+            for (int i = Posicao.Linha; i < Tabuleiro.Linha; i++)
+            {
+                Tabuleiro.Posicao posicao = new(i + 1, i - 1);
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+
+                posicao.Coluna = i + 1;
+
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            return movimentosPossiveis;
         }
 
         public override string ToString() => "D";
