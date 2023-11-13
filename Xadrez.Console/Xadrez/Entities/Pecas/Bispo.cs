598b0d2fed8f8ce22a5a08d52f0cce31f68ad2cf
Tiago Pires <tiagopgu.pires@gmail.com>
2023-11-13 12:58:21 -0300

    Corrigindo movimentos possíveis do bispo para evitar que salte sobre outras peças

--- Xadrez.Console/Xadrez/Entities/Pecas/Bispo.cs
+++ Xadrez.Console/Xadrez/Entities/Pecas/Bispo.cs
@@ -14,35 +14,73 @@ namespace Xadrez.ConsoleApp.Xadrez.Entities.Pecas
         {
             var movimentosPossiveis = new bool[Tabuleiro.Linha, Tabuleiro.Coluna];
 
-            // Vertical Superior
-            for (int i = Posicao.Linha; i > 0; i--)
-            {
-                Tab.Posicao posicao = new(i - 1, i - 1);
-
-                if (ValidarMovimento(posicao))
-                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
-
-                posicao.Coluna = i + 1;
-
-                if (ValidarMovimento(posicao))
-                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
-            }
-
-            // Vertical Inferior
-            for (int i = Posicao.Linha; i < Tabuleiro.Linha; i++)
-            {
-                Tab.Posicao posicao = new(i + 1, i - 1);
-
-                if (ValidarMovimento(posicao))
-                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
-
-                posicao.Coluna = i + 1;
-
-                if (ValidarMovimento(posicao))
-                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
-            }
+            MarcarMovimentosVerticalSuperiorEsquerdo(movimentosPossiveis);
+            MarcarMovimentosVerticalSuperioDireito(movimentosPossiveis);
+            MarcarMovimentosVerticalInferiorDireito(movimentosPossiveis);
+            MarcarMovimentosVerticalInferiorEsquerdo(movimentosPossiveis);
 
             return movimentosPossiveis;
+        }
+
+        private void MarcarMovimentosVerticalSuperiorEsquerdo(bool[,] movimentosPossiveis)
+        {
+            for (int i = Posicao.Linha - 1; i >= 0; i--)
+            {
+                var qtdLinhasCaminhadas = Posicao.Linha - i;
+                Tab.Posicao posicao = new(i, Posicao.Coluna - qtdLinhasCaminhadas);
+
+                if (!TratarPosicaoAtual(movimentosPossiveis, posicao))
+                    break;
+            }
+        }
+
+        private void MarcarMovimentosVerticalSuperioDireito(bool[,] movimentosPossiveis)
+        {
+            for (int i = Posicao.Linha - 1; i >= 0; i--)
+            {
+                var qtdLinhasCaminhadas = Posicao.Linha - i;
+                Tab.Posicao posicao = new(i, Posicao.Coluna + qtdLinhasCaminhadas);
+
+                if (!TratarPosicaoAtual(movimentosPossiveis, posicao))
+                    break;
+            }
+        }
+
+        private void MarcarMovimentosVerticalInferiorDireito(bool[,] movimentosPossiveis)
+        {
+            for (int i = Posicao.Linha + 1; i < Tabuleiro.Linha; i++)
+            {
+                var qtdLinhasCaminhadas = i - Posicao.Linha;
+                Tab.Posicao posicao = new(i, Posicao.Coluna + qtdLinhasCaminhadas);
+
+                if (!TratarPosicaoAtual(movimentosPossiveis, posicao))
+                    break;
+            }
+        }
+
+        private void MarcarMovimentosVerticalInferiorEsquerdo(bool[,] movimentosPossiveis)
+        {
+            for (int i = Posicao.Linha + 1; i < Tabuleiro.Linha; i++)
+            {
+                var qtdLinhasCaminhadas = i - Posicao.Linha;
+                Tab.Posicao posicao = new(i, Posicao.Coluna - qtdLinhasCaminhadas);
+
+                if (!TratarPosicaoAtual(movimentosPossiveis, posicao))
+                    break;
+            }
+        }
+
+        private bool TratarPosicaoAtual(bool[,] movimentosPossiveis, Tab.Posicao posicao)
+        {
+            if (!ValidarMovimento(posicao))
+                return false;
+
+            movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+
+            if (ValidarSeExistePecaAdversaria(posicao))
+                return false;
+
+            return true;
         }
 
         public override string ToString() => "B";
