542eaa9c9a2ca39f91ed52983b208d70ba2c1de2
Tiago Pires <tiagopgu.pires@gmail.com>
2023-11-13 00:26:37 -0300

    Implementando movimentos possíveis do cavalo.

--- Xadrez.Console/Xadrez/Pecas/Cavalo.cs
+++ Xadrez.Console/Xadrez/Pecas/Cavalo.cs
@@ -12,7 +12,40 @@ namespace Xadrez.ConsoleApp.Xadrez.Pecas
 
         public override bool[,] RetornarMovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            var movimentosPossiveis = new bool[Tabuleiro.Linha, Tabuleiro.Coluna];
+
+            void atualizarMovimentosPossiveis(Tabuleiro.Posicao posicao)
+            {
+                if (ValidarMovimento(posicao))
+                    movimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            for (int i = Posicao.Linha - 2; i <= Posicao.Linha + 2; i++)
+            {
+                if (i == Posicao.Linha - 2 || i == Posicao.Linha + 2)
+                {
+                    Tabuleiro.Posicao posicao = new(i, Posicao.Coluna - 1);
+
+                    atualizarMovimentosPossiveis(posicao);
+
+                    posicao.Coluna = Posicao.Coluna + 1;
+
+                    atualizarMovimentosPossiveis(posicao);
+                }
+                else
+                {
+                    Tabuleiro.Posicao posicao = new(i, Posicao.Coluna - 2);
+
+                    atualizarMovimentosPossiveis(posicao);
+
+                    posicao.Coluna = Posicao.Coluna + 2;
+
+                    atualizarMovimentosPossiveis(posicao);
+                }
+
+            }
+
+            return movimentosPossiveis;
         }
 
         public override string ToString() => "C";
